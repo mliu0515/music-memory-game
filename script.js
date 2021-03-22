1e452feee171fe5db9ba5bd882221ba577967b08
Glitch (polyester-cumbersome-deposit) <none>
2021-03-22 19:49:56 +0000

    🐈💸 Updated with Glitch

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // global constants
-const clueHoldTime = 1000; //how long to hold each clue's light/sound
+
 const cluePauseTime = 333; //how long to pause in between clues
 const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
+const totalStrikesAllowed = 3; //this gives player 3 chances
 
 //Global Variables
 var pattern = [1, 1, 4, 3, 2, 3, 4, 3];
@@ -10,11 +11,14 @@ var gamePlaying = false;
 var tonePlaying = false;
 var volume = 0.5;  //must be between 0.0 and 1.0
 var guessCounter = 0;
+var clueHoldTime = 1000; //how long to hold each clue's light/sound
+var currStrike = 0;
 
 function startGame(){
   //initialize game variables
   progress = 0;
   gamePlaying = true;
+  currStrike = 0; // current number of strikes is equal to 0
   // swap the Start and Stop buttons
   document.getElementById("startBtn").classList.add("hidden");
   document.getElementById("stopBtn").classList.remove("hidden");
@@ -55,6 +59,7 @@ function playClueSequence(){
     setTimeout(playSingleClue,delay,pattern[i]) // set a timeout to play that clue
     delay += clueHoldTime 
     delay += cluePauseTime;
+    clueHoldTime -= 20;
   }
 }
 
@@ -79,10 +84,14 @@ function guess(btn){
 
   // add game logic here
   var correct = pattern[guessCounter];
-  if (correct != btn) {
+  if (correct != btn && currStrike == totalStrikesAllowed) {
     loseGame();
   } else {
-    if (guessCounter == progress) {
+    if (correct != btn) { //if the guess is wrong but we still have more chances
+      currStrike += 1; //increment the strike by 1
+      alert("oops, you last note is wrong. Guess the last note again."); //alert
+    } else { // else if we made a correct guess
+      if (guessCounter == progress) {
       if (progress == pattern.length - 1) {
         winGame();
       } else {
@@ -92,6 +101,7 @@ function guess(btn){
     } else {
       guessCounter += 1;
     }
+  }
   }
 }
 // Sound Synthesis Functions
